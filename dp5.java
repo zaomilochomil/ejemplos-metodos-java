156c3629c1d4ea1bbc5bea8a9b60b76a37a873ed
zaomilochomil <162t0310@itsm.edu.mx>
2017-02-11 17:08:42 -0600

    Create dp5.java

---
+++ dp5.java
@@ -0,0 +1,14 @@
+package D;
+
+/**
+ *
+ * @author saul
+ */
+public class P5 {
+    public static void main (String [] args){
+ System.out.println("El cubo de 7.5 es: " + cubo(7.5));
+ }
+ public static double cubo (double x) {
+ return x*x*x;
+ }
+}
